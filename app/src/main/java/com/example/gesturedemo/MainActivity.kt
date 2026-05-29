df50c9fe3cc90b0f262d180047ae51736e385cbc
demyop <daniil.moiseenco11@gmail.com>
2026-05-29 20:57:05 +0700

    Tap_Press_Demo

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -3,45 +3,116 @@ package com.example.gesturedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.gesturedemo.ui.theme.GesturedemoTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.runtime.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            GesturedemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    TapPressDemo(modifier)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GesturedemoTheme {
-        Greeting("Android")
+fun ClickDemo(modifier: Modifier = Modifier) {
+
+    var colorState by remember { mutableStateOf(true) }
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+
+    val clickHandler = {
+
+        colorState = !colorState
+
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Box(
+            Modifier
+                .clickable { clickHandler() }
+                .background(bgColor)
+                .size(100.dp)
+        )
+    }
+}
+@Composable
+fun TapPressDemo(modifier: Modifier = Modifier) {
+
+    var textState by remember {
+        mutableStateOf("Waiting ....")
+    }
+
+    val tapHandler = { status: String ->
+        textState = status
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        Box(
+            Modifier
+                .padding(10.dp)
+                .background(Color.Blue)
+                .size(100.dp)
+                .pointerInput(Unit) {
+
+                    detectTapGestures(
+
+                        onPress = {
+                            tapHandler("onPress Detected")
+                        },
+
+                        onDoubleTap = {
+                            tapHandler("onDoubleTap Detected")
+                        },
+
+                        onLongPress = {
+                            tapHandler("onLongPress Detected")
+                        },
+
+                        onTap = {
+                            tapHandler("onTap Detected")
+                        }
+                    )
+                }
+        )
+
+        Spacer(Modifier.height(10.dp))
+
+        Text(textState)
     }
 }

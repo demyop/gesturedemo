00a43c0e16a70cc5ba1c1383d116ba8b53dd02cc
demyop <daniil.moiseenco11@gmail.com>
2026-05-31 02:34:31 +0700

    DragDemo

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -5,22 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.example.gesturedemo.ui.theme.GesturedemoTheme
-import androidx.compose.ui.Alignment
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.background
-import androidx.compose.runtime.*
-import androidx.compose.material3.Text
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    TapPressDemo(modifier)
+    DragDemo(modifier)
 }
 
 @Composable
@@ -91,19 +101,15 @@ fun TapPressDemo(modifier: Modifier = Modifier) {
                 .pointerInput(Unit) {
 
                     detectTapGestures(
-
                         onPress = {
                             tapHandler("onPress Detected")
                         },
-
                         onDoubleTap = {
                             tapHandler("onDoubleTap Detected")
                         },
-
                         onLongPress = {
                             tapHandler("onLongPress Detected")
                         },
-
                         onTap = {
                             tapHandler("onTap Detected")
                         }
@@ -112,7 +118,31 @@ fun TapPressDemo(modifier: Modifier = Modifier) {
         )
 
         Spacer(Modifier.height(10.dp))
-
         Text(textState)
+    }
+}
+
+@Composable
+fun DragDemo(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        var xOffset by remember {
+            mutableStateOf(0f)
+        }
+        Box(
+            modifier = Modifier
+                .offset {
+                    IntOffset(xOffset.roundToInt(), 0)
+                }
+                .size(100.dp)
+                .background(Color.Blue)
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { distance ->
+                        xOffset += distance
+                    }
+                )
+        )
     }
 }
